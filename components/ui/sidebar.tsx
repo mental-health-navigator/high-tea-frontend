59114445e594558f3cc9004dfd2e11d0a3d10f06
jinglegordonguo <jinglegordonguo@gmail.com>
2025-10-25 16:43:33 +1100

    fix: react aria warning

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -10,8 +10,9 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
-import { Sheet, SheetContent } from '@/components/ui/sheet';
+import { Sheet, SheetContent, SheetTitle } from '@/components/ui/sheet';
 import { Skeleton } from '@/components/ui/skeleton';
+import { VisuallyHidden } from '@/components/ui/visually-hidden';
 import {
   Tooltip,
   TooltipContent,
@@ -215,6 +216,9 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+            <VisuallyHidden>
+              <SheetTitle>Sidebar</SheetTitle>
+            </VisuallyHidden>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>

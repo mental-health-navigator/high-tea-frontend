6474231adf9c641b3d585664dbeab53d7980e11c
jingle-unimelb <jingleg@student.unimelb.edu.au>
2025-10-25 15:05:37 +1100

    chore: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,9 @@ import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  metadataBase: new URL('https://high-tea-frontend.vercel.app/'),
+  title: 'Heal Hub',
+  description: 'Heal Hub health chatbot',
 };
 
 export const viewport = {
